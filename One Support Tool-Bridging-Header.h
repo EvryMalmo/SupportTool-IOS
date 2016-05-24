47199381276c4c94977e3783c95b2c130f64944d
Evry Malmö <Admin@evrys-macbook-air.local>
2016-05-24 13:32:55 +0200

    Initial Commit

---
+++ One Support Tool-Bridging-Header.h
@@ -0,0 +1,16 @@
+//
+//  One Support Tool-Bridging-Header.h
+//  One Support Tool
+//
+//  Created by Admin on 2016-02-10.
+//  Copyright © 2016 EVRY. All rights reserved.
+//
+
+#ifndef One_Support_Tool_Bridging_Header_h
+#define One_Support_Tool_Bridging_Header_h
+
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <ifaddrs.h>
+
+#endif /* One_Support_Tool_Bridging_Header_h */
---
+++ One Support Tool/RemoteContactViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  One Support Tool
+//
+//  Created by Admin on 2016-02-04.
+//  Copyright © 2016 EVRY. All rights reserved.
+//
+
+import UIKit
+
+class RemoteContactViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
